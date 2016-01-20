e76420385737117555609a791bef2297bebfda48
kaitlingu <kaitlin.gu@gmail.com>
2016-01-20 04:10:59 -0500

    typing

---
+++ js/typing.js
@@ -0,0 +1,11 @@
+ $(function(){
+      $(".test").typed({
+      strings: ["musicians", "programmers", "artists", "scientists", "composers","hardware tinkerers", "and others"],
+      typeSpeed: 50,
+      backSpeed: 50,
+      showCursor: false,
+      loop: true,
+      loopCount: false,
+      backDelay: 1000,
+      });
+      });

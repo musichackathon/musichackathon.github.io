541b2533bbed5ddbe146e752638ffc355928d8be
Ya-wen Alice <yawenalice@gmail.com>
2017-03-19 12:32:54 -0400

    added template

--- index.js
+++ index.js
@@ -1,3 +1,24 @@
-$(document).ready(function() {
-  console.log('hello');
+$(function() {
+
+  $.addTemplateFormatter({
+
+  });
+
+  //call to JSON file location
+  (function() {
+    const fileLocation = 'nextEvent.json'
+    $.getJSON(fileLocation, function(data) {
+console.log('DATA', data)
+
+$('#nextEvent').loadTemplate($('#template'), data, {
+
+      success: function() {console.log('yay')},
+      complete: function(){ console.log('complete')},
+      error: function() {console.log('error')}
+    })
+})
+  })
+
+
+
 });
